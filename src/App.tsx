edb752fa3972ea23c764fa6288f2b3bf94272550
Nurlianto Aldi <nurliantoaldi@gmail.com>
2025-10-26 17:03:23 +0700

    add custom font and install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      this is working
+      <h1
+        className='text-4xl'
+      >
+        this is working
+      </h1>
+      <h1
+        className='text-4xl font-roboto'
+      >
+        this is working
+      </h1>
+      <h1
+        className='text-4xl font-roboto-mono'
+      >
+        this is working
+      </h1>
       
     </div>
   )
